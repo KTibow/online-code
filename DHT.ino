738fe74fdc9cb56ce66e75357e8aa688066854c4
kendell <kendell.r@outlook.com>
2015-05-02 14:16:36 -0700

    Update DHT.ino

--- DHT.ino
+++ DHT.ino
@@ -2,3 +2,9 @@
 #include <TinyLiquidCrystal.h>
 #include <TinyDHT.h>
 #include <avr/power.h>
+
+#define DHTTYPE DHT22
+#define TEMPTYPE 1
+#define DHTPIN 1
+DHT dht(DHTPIN, DHTTYPE);
+TinyLiquidCrystal lcd(0);
