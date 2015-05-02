4e2fb97303fa05ab88a2c8555181ed7aeec386eb
kendell <kendell.r@outlook.com>
2015-05-02 14:04:57 -0700

    Update hello_world_Backpack.ino

--- hello_world_Backpack.ino
+++ hello_world_Backpack.ino
@@ -4,5 +4,11 @@ TinyLiquidCrystal lcd(0);
 
 void setup() {
   lcd.begin(16, 2);
-  lcd.setBacklight
+  lcd.setBacklight(HIGH);
+  lcd.print("hello, World!");
+}
+
+void loop() {
+  lcd.setCursor(0, 1);
+  lcd.print(millis()/1000);
 }
